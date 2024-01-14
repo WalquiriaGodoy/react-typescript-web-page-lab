911ceadbaed091c6715524298115258b86e19896
Walquiria Godoy <walquiriagodoy@gmail.com>
2024-01-14 20:10:10 -0300

    feat: adjust error message

--- src/Componentes/Formulário/LeadForm/LeadForm.tsx
+++ src/Componentes/Formulário/LeadForm/LeadForm.tsx
@@ -2,9 +2,28 @@ import { Input } from 'src/design-system/Input';
 import { SubmitButton } from 'src/design-system/SubmitButton';
 import { useForm } from 'react-hook-form';
 import * as S from './LeadForm.styles';
+import z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export const LeadForm = () => {
-  const { register, handleSubmit } = useForm();
+  type createLeadDataProps = z.infer<typeof createLeadSchema>;
+
+  const createLeadSchema = z.object({
+    name: z.string().min(1, 'O nome é obrigatório'),
+    surname: z.string().min(1, 'O sobrenome é obrigatório'),
+    email: z.string().min(1, 'O email é obrigatório').email('Email inválido'),
+    telephone: z.string().min(1, 'O whatsapp é obrigatório'),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<createLeadDataProps>({
+    resolver: zodResolver(createLeadSchema),
+  });
+
+  console.log('ERRORS', errors);
 
   const createLead = (data: any) => {
     console.log(data);
@@ -12,10 +31,30 @@ export const LeadForm = () => {
 
   return (
     <S.LeadFormContainer onSubmit={handleSubmit(createLead)}>
-      <Input label='Nome' type='name' register={register} />
-      <Input label='Sobrenome' type='surname' register={register} />
-      <Input label='Email' type='email' register={register} />
-      <Input label='Whatsapp' type='telephone' register={register} />
+      <Input
+        label='Nome'
+        type='name'
+        register={register}
+        message={errors.name?.message}
+      />
+      <Input
+        label='Sobrenome'
+        type='surname'
+        register={register}
+        message={errors.surname?.message}
+      />
+      <Input
+        label='Email'
+        type='email'
+        register={register}
+        message={errors.email?.message}
+      />
+      <Input
+        label='Whatsapp'
+        type='telephone'
+        register={register}
+        message={errors.telephone?.message}
+      />
 
       <SubmitButton text='Enviar' />
     </S.LeadFormContainer>
--- src/design-system/Input/Input.styles.ts
+++ src/design-system/Input/Input.styles.ts
@@ -14,4 +14,11 @@ export const LabelContainer = styled.label`
 export const InputContainer = styled.input`
   border-radius: 5px;
   height: 2rem;
+  padding: 0.2rem 0.4rem;
+`;
+
+export const MessageContainer = styled.span`
+  color: red;
+  font-size: 1rem;
+  margin-top: 0.2rem;
 `;
